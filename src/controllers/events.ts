221bed87ca988faefe496e03860d78d11a2e6aa7
wigor <wigor21@hotmail.com>
2024-02-05 22:05:10 -0300

    feat: Create controller to list all events

---
+++ src/controllers/events.ts
@@ -0,0 +1,10 @@
+import { RequestHandler } from "express";
+import * as events from "../services/events";
+
+export const getAll: RequestHandler = async (req, res) => {
+  const items = await events.getAll();
+
+  if (items) return res.json({ events: items });
+
+  res.json({ error: "Ocorreu um erro" });
+};
--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as auth from "../controllers/auth";
+import * as events from "../controllers/events";
 
 const router = Router();
 
@@ -8,5 +9,7 @@ router.post("/login", auth.login);
 router.get("/ping", auth.validate, (req, res) =>
   res.json({ pong: true, admin: true })
 );
+
+router.get("/events", auth.validate, events.getAll);
 
 export default router;
