91fc4b1cb0aeb648e7615b9a6ea3f3bafba834c3
wigor <wigor21@hotmail.com>
2024-02-11 01:23:17 -0300

    feat: Create a controller to retrieve the selected person and their match

--- src/controllers/people.ts
+++ src/controllers/people.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import * as people from "../services/people";
 import { string, z } from "zod";
+import { decryptMatch } from "../utils/match";
 
 export const getAll: RequestHandler = async (req, res) => {
   const { id_event, id_group } = req.params;
@@ -98,6 +99,47 @@ export const deletePerson: RequestHandler = async (req, res) => {
   });
 
   if (deletedPerson) return res.json({ person: deletedPerson });
+
+  res.json({ erros: "Ocorreu um erro" });
+};
+
+export const searchPerson: RequestHandler = async (req, res) => {
+  const { id_event } = req.params;
+
+  const searchPersonSchema = z.object({
+    cpf: z.string().transform((val) => val.replace(/\.|-/gm, "")),
+  });
+
+  const query = searchPersonSchema.safeParse(req.query);
+
+  if (!query.success) return res.json({ error: "Dados inválidos" });
+
+  const personItem = await people.getOne({
+    id_event: Number(id_event),
+    cpf: query.data.cpf,
+  });
+
+  if (personItem && personItem.matched) {
+    const matchId = decryptMatch(personItem.matched);
+
+    const personMatched = await people.getOne({
+      id_event: Number(id_event),
+      id: matchId,
+    });
+
+    if (personMatched) {
+      return res.json({
+        person: {
+          id: personItem.id,
+          name: personItem.name,
+        },
+        personMatched: {
+          id: personMatched.id,
+          name: personMatched.name,
+        },
+      });
+    }
+  }
 
   res.json({ erros: "Ocorreu um erro" });
 };
